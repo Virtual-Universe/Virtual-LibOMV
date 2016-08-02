2dc82203f00943b058e904ae5aa5b8e0edf23aa7
EmperorStarfinder <emperor@secondgalaxy.com>
2016-08-02 19:10:12 -0400

    Update AssemblyInfo to Version 1.0.2.2
    Signed-Off-By: EmperorStarfinder<emperor@secondgalaxy.com>

--- Programs/examples/TestClient/Properties/AssemblyInfo.cs
+++ Programs/examples/TestClient/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.4.5")]
-[assembly: AssemblyFileVersion("0.9.4.5")]
+[assembly: AssemblyVersion("1.0.2.2")]
+[assembly: AssemblyFileVersion("1.0.2.2")]
